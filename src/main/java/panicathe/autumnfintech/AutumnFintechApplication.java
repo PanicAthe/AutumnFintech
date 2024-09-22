a907e1f2e631e2511e69aff85099584c34fb7fde
jin <ekdekdgkrp20@gmail.com>
2024-09-22 20:14:18 +0900

    Update Poject Skeleton

---
+++ src/main/java/panicathe/autumnfintech/AutumnFintechApplication.java
@@ -0,0 +1,13 @@
+package panicathe.autumnfintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnFintechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutumnFintechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panicathe/autumnfintech/AutumnFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package panicathe.autumnfintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutumnFintechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
